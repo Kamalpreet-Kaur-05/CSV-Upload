0cf66fe75c0a52b39c7b199f3b2f3fe598f5e872
Kamalpreet-Kaur-05 <132252632+Kamalpreet-Kaur-05@users.noreply.github.com>
2023-08-22 11:08:42 +0530

    update

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,17 +1,29 @@
 //require the library
 const mongoose = require('mongoose');
-require('dotenv').config()
-//connect to the db
-mongoose.connect(`mongodb://127.0.0.1:27017/CSV-Upload`);
-//mongoose.connect(process.env.mongooseUrl);
-//require connection
+const DB = 'mongodb+srv://kkamalpreet847:df1oreNkuZ4O0Oe1@csvproject.si7etoe.mongodb.net/CSV-Upload?retryWrites=true&w=majority';
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/csvUploader');
+
+// mongoose.connect(DB, {
+// 	useNewUrlParser: true,
+// 	useCreateIndex:true,
+//  	useUnifiedTopology: true,
+// 	useFindAndModify:false
+// }).then(() => {
+// 	console.log('connection successful');
+// }).catch((err) => console.log('no connection',err));
+
+
+mongoose.connect(DB, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
 const db = mongoose.connection;
 
-// if error
-db.on('error', console.error.bind(console, 'Error Connecting to Database!'));
-//if server up then print this meassage
-db.once('open', function() {
-  console.log("Successfully Connected to Database :: MongoDB");
+db.on('error', console.error.bind(console, 'Error in connecting to MongoDB'));
+
+db.once('open', function () {
+	console.log('Connected to Database :: Mongodb');
 });
 
 module.exports = db;
